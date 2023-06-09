08118ba8c8fba0231241688f1c7e1d25f87d9f7f
mmcaulif <60351960+mmcaulif@users.noreply.github.com>
2023-06-09 23:02:21 +0200

    began work on minari/offline gatherer

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Immediate to do list
 * Offline gatherer
 
+* refactor gatherer and runner, add default arg values, careful consideration needed
 
 * Make repo a package (just a setup file for now)
     * need to debug and make cleaner, go through documentation
---
+++ minari_testing.py
@@ -0,0 +1,75 @@
+from collections import deque
+from src import Collector, Runner
+from src.policies import Base_policy
+from minari import DataCollectorV0
+import minari
+import gymnasium as gym
+
+class OfflineCollector(Collector):
+    def __init__(self, env, capacity) -> None:
+        super().__init__(env, capacity)
+        self.env = env
+
+    def warmup(self, net=None, policy=None, length=1000000, n_step=1):
+
+        if policy == None:
+            policy = Base_policy(self.env)
+
+        self.env = DataCollectorV0(self.env, record_infos=False, max_buffer_steps=length)
+        self.net = net
+        gather_buffer = deque()
+        step_buffer = deque(maxlen=n_step)
+
+        if length < 1:
+            print('cant have n-step less than 1 when using offline gatherer')      
+
+        for _ in range(length):
+            self.total_steps += 1
+            a = policy(self.state, self.net)
+            s_p, r, d, t, info = self.env.step(a)
+
+            step_buffer.append([self.state, a, r, s_p, d])
+            if len(step_buffer) == n_step:
+
+                if n_step == 1:
+                    gather_buffer.append(*list(step_buffer))
+                else:
+                    gather_buffer.append(list(step_buffer))
+
+            self.state = s_p
+            if d or t:
+                self.episodes += 1
+                self.epsiode_window.append(self.ep_rew)
+                self.state, _ = self.env.reset()
+                step_buffer = deque(maxlen=n_step)
+
+        self._set_up_loader(data=list(gather_buffer))
+
+        return list(gather_buffer)
+    
+    def _set_up_loader(self, data):
+        print(len(data))
+        dataset = minari.create_dataset_from_collector_env(dataset_id="CartPole-v1-test-v0", 
+                                                   collector_env=self.env)
+
+def get_offline_runner(env, capacity):
+    return Runner(
+        env,
+        Base_policy(env),
+        length=1,
+        sampler=False,
+        capacity=0,
+        batch_size=256,
+        n_step=1,
+        collector=OfflineCollector,
+        train_after=capacity,
+    )
+
+def main():
+    env = gym.make('CartPole-v1')
+    runner = get_offline_runner(env, 200)
+
+    pass
+
+if __name__ == '__main__':
+    main()
