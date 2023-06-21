3aae5db536b1f76000db61b636767daecad5a829
mmcaulif <mmcaulif@tcd.ie>
2023-06-21 10:50:39 +0200

    added info and flexibility to named tuple

--- cardio_rl/transitions/transitions.py
+++ cardio_rl/transitions/transitions.py
@@ -18,11 +18,12 @@ of the Transition class itself? That could be a good fix
 """
 
 class BaseTransition(NamedTuple):
-    s: list  # state
-    a: float  # action
-    r: float  # reward
-    s_p: list  # next state
+    s: list # state
+    a: float    # action
+    r: float    # reward
+    s_p: list   # next state
     d: int  # done
+    i: dict #info
 
     def __call__(self):
         s = self.s
@@ -30,25 +31,32 @@ class BaseTransition(NamedTuple):
         r = self.r
         s_p = self.s_p
         d = self.d
-        return s, a, r, s_p, d
+        i = self.i
+        return s, a, r, s_p, d, i
 
 class TorchTransition(BaseTransition):
 
-    def __call__(self):
+    def __call__(self, info=False):
         s = th.from_numpy(np.array(self.s)).float()
         a = th.from_numpy(np.array(self.a)).unsqueeze(1).float()
         r = th.from_numpy(np.array(self.r)).unsqueeze(1).float()
         s_p = th.from_numpy(np.array(self.s_p)).float()
         d = th.from_numpy(np.array(self.d)).unsqueeze(1).int()
+        if info:
+            return s, a, r, s_p, d, self.i
+
         return s, a, r, s_p, d
     
 class JaxTransition(BaseTransition):
 
-    def __call__(self):
+    def __call__(self, info=False):
         s = jnp.asarray(np.array(self.s))
         a = jnp.asarray(np.array(self.a))
         r = jnp.expand_dims(jnp.asarray(np.array(self.r)), -1)
         s_p = jnp.asarray(np.array(self.s_p))
         d = jnp.expand_dims(jnp.asarray(np.array(self.d)), -1)
+        if info:
+            return s, a, r, s_p, d, self.i
+
         return s, a, r, s_p, d
     
