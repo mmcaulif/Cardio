3a56080ef1259b38d8dabe2549cfd0bf446dc143
mmcaulif <mmcaulif@tcd.ie>
2023-06-23 20:09:56 +0200

    mpo impl. sort of performing and stable, still wip

--- mpo_testing.py
+++ mpo_testing.py
@@ -3,19 +3,30 @@ import torch as th
 import torch.nn as nn
 import torch.nn.functional as F
 import gymnasium as gym
-import numpy as np
 from cardio_rl import Runner
 from cardio_rl import Collector
-from main import ImplicitQ
 
 """
 To do:
--implement e-pochs for E and M step, figure out how many
--implement kl-div penalty for actor loss
+-figure out how many e-pochs for E and M step
 -implement alpha dual loss
--make sure kl-div is calculated right
--figure out where to use target actor, presume in weight calculation DOUBLE CHECK AND MAKE SURE
+-look into what hyperparams to use, maybe good time to try optuna
+-look into Acme impl. for how to handle target updates, i.e. freq
+ of hard updates or value for tau
 """
+
+class Critic(nn.Module):
+	def __init__(self, state_dim, action_dim):
+		super(Critic, self).__init__()
+		self.net = nn.Sequential(
+			nn.Linear(state_dim, 64),
+			nn.ReLU(),
+			nn.Linear(64, 64),
+			nn.ReLU(),
+			nn.Linear(64, action_dim))
+
+	def forward(self, state):
+		return self.net(state)
 
 class Actor(nn.Module):
 	def __init__(self, state_dim, action_dim):
@@ -47,35 +58,34 @@ runner = Runner(
 	backend='pytorch'
 )
 
-critic = ImplicitQ(4, 2)	# move to using double duelling dqn
+critic = Critic(4, 2)	# move to using double duelling dqn
 actor = Actor(4, 2)
 
 targ_critic = copy.deepcopy(critic)
 targ_actor = copy.deepcopy(actor)
 
-lr = 7e-4
+lr = 3e-4
 
-optimizer = th.optim.Adam(critic.parameters(), lr=lr)
-actor_optimizer = th.optim.Adam(actor.parameters(), lr=lr)
+critic_optim = th.optim.Adam(critic.parameters(), lr=lr)
+actor_optim = th.optim.Adam(actor.parameters(), lr=lr)
 gamma = 0.99
 target_update = 10
 
 eps = 0.1
 
-# check init values of lagrangian multipliers
-eta = nn.Parameter(th.ones(1)*0.03)
+# check init values of lagrangian multipliers, also look into the projection etc.
+eta = nn.Parameter(th.ones(1))
 eta_optim = th.optim.Adam([eta], lr=lr)
 
-# check init values of lagrangian multipliers
-alpha = nn.Parameter(th.ones(1)*0.03)
+alpha = th.tensor(0.2, requires_grad=True)	# normally 0.1
 alpha_optim = th.optim.Adam([alpha], lr=lr)
 
 for t in range(10000):
 	batch = runner.get_batch(actor)
 	s, a, r, s_p, d = batch()
 
+	### Policy evaluation
 	q = critic(s).gather(1, a.long())
-
 	with th.no_grad():
 		a_p = critic(s_p).argmax(dim=1).unsqueeze(1)		
 		q_p = targ_critic(s_p).gather(1, a_p.long())		
@@ -83,45 +93,47 @@ for t in range(10000):
 
 	loss = F.mse_loss(q, y)
 
-	optimizer.zero_grad()
+	critic_optim.zero_grad()
 	loss.backward()
-	optimizer.step()
+	nn.utils.clip_grad_norm_(critic.parameters(), 5.0)
+	critic_optim.step()
 	
 	### E-step
-
 	for _ in range(5):
-		energies = th.exp(critic(s).detach() / eta)
-
-		# logits = actor(s).detach() * energies
-		logits = targ_actor(s).detach() * energies
-
-		logits = logits.mean(-1)
-		logits = th.log(logits).mean()
-		temp_loss = (eta * (eps + logits))
+		energies = th.exp(critic(s).detach() / eta).mean(-1)
+		logits = th.log(energies).mean()
+		eta_loss = (eta * (eps + logits))
 
 		eta_optim.zero_grad()
-		temp_loss.backward()
+		eta_loss.backward()		
 		eta_optim.step()
-
-	q_values = critic(s).detach()
-	qij = F.softmax(q_values/eta.detach(), dim=-1)
-
-	# print(f'non-para: {qij[0].numpy()}, policy: {actor(s)[0].detach().numpy()}, q vals: {critic(s)[0].detach().numpy()}')
+	
+	qij = F.softmax(targ_critic(s) / eta, dim=-1).detach()
+	prior_probs = targ_actor(s).detach()
 
 	### M-step
-	probs = actor(s)
-	# check kl div calculation
-	a_loss = th.sum(qij * th.log(probs/qij), dim=-1)
-	a_loss = th.mean(a_loss)
-	# kl = th.mean(th.sum(probs * th.log(targ_actor(s).detach()), dim=-1))
+	for _ in range(5):
+		probs = actor(s)
+		
+		# check kl div calculation
+		loss_p = th.mean(qij * th.log(probs))
 
-	loss_p = -(a_loss)	#  + 0.5 * (0.01 - kl))
+		# check dimensions of kl-div!
+		kl = th.mean((probs * th.log(probs / prior_probs)).sum(dim=-1))
+		total_loss_p = -(loss_p + alpha.detach() * (eps - kl))
 
-	actor_optimizer.zero_grad()
-	loss_p.backward()
-	# th.nn.utils.clip_grad_norm_(actor.parameters(), 0.1)
-	actor_optimizer.step()
-	
-	if t % 10 == 0:        
-		targ_critic = copy.deepcopy(critic)
-		targ_actor = copy.deepcopy(actor)
+		actor_optim.zero_grad()
+		total_loss_p.backward()
+		nn.utils.clip_grad_norm_(actor.parameters(), 5.0)
+		actor_optim.step()
+
+		# alpha currently not set to autotune
+		continue
+		alpha_loss = alpha * (eps - kl).detach()
+		alpha_optim.zero_grad()
+		alpha_loss.backward()
+		alpha_optim.step()
+
+	# implement soft updates maybe?
+	targ_critic = copy.deepcopy(critic)
+	targ_actor = copy.deepcopy(actor)
