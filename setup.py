7f9d8d1b3834ccf2a1a0ac4d915639dac424319a
mmcaulif <mmcaulif@tcd.ie>
2023-07-12 22:12:50 +0200

    updating setup

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     
     # PyPI package information.
     classifiers=[
-        'Development Status :: 3 - Alpha'
+        'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
