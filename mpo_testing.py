e13a84ab82bff7e193cbd4b191a42288df071377
mmcaulif <mmcaulif@tcd.ie>
2023-07-14 17:29:56 +0200

    continuing to try MPO

--- mpo_testing.py
+++ mpo_testing.py
@@ -9,21 +9,24 @@ from cardio_rl import Collector
 """
 To do:
 -figure out how many e-pochs for E and M step
--implement alpha dual loss
 -look into what hyperparams to use, maybe good time to try optuna
--look into Acme impl. for how to handle target updates, i.e. freq
- of hard updates or value for tau
+-look into Acme impl. 
+Resources:
+-https://github.com/ethanluoyc/magi/pull/59
+-https://github.com/vwxyzjn/cleanrl/pull/392
+-https://github.com/daisatojp/mpo/blob/master/mpo/mpo.py
+-https://github.com/deepmind/acme/tree/master/acme/agents/jax/mpo
 """
 
 class Critic(nn.Module):
 	def __init__(self, state_dim, action_dim):
 		super(Critic, self).__init__()
 		self.net = nn.Sequential(
-			nn.Linear(state_dim, 64),
+			nn.Linear(state_dim, 256),
 			nn.ReLU(),
-			nn.Linear(64, 64),
+			nn.Linear(256, 256),
 			nn.ReLU(),
-			nn.Linear(64, action_dim))
+			nn.Linear(256, action_dim))
 
 	def forward(self, state):
 		return self.net(state)
@@ -32,80 +35,98 @@ class Actor(nn.Module):
 	def __init__(self, state_dim, action_dim):
 		super(Actor, self).__init__()
 		self.net = nn.Sequential(
-			nn.Linear(state_dim, 64),
+			nn.Linear(state_dim, 256),
 			nn.ReLU(),
-			nn.Linear(64, 64),
+			nn.Linear(256, 256),
 			nn.ReLU(),
-			nn.Linear(64, action_dim),
+			nn.Linear(256, action_dim),
 			nn.Softmax(dim=-1))
 
 	def forward(self, state):
 		return self.net(state)
 
-env = gym.make('CartPole-v1')
+env = gym.make('LunarLander-v2')
 
 runner = Runner(
 	env=env,
-	policy='categorical',	# Epsilon_argmax_policy(env, 0.5, 0.05, 0.9),
+	policy='categorical',
 	sampler=True,
-	capacity=1_000_000,
+	capacity=50_000,	# 1_000_000,
 	batch_size=256,
 	collector=Collector(
 		env=env,
 		rollout_len=4,
-		warmup_len=1000,
+		warmup_len=1_000,
+		logger_kwargs=dict(
+			log_interval=5000,
+		)
 	),
 	backend='pytorch'
 )
 
-critic = Critic(4, 2)	# move to using double duelling dqn
-actor = Actor(4, 2)
+critic = Critic(8, 4)
+actor = Actor(8, 4)
 
 targ_critic = copy.deepcopy(critic)
 targ_actor = copy.deepcopy(actor)
 
-lr = 3e-4
+# taken from acme
+lr = 1e-3
+dual_lr = 1e-3
 
 critic_optim = th.optim.Adam(critic.parameters(), lr=lr)
 actor_optim = th.optim.Adam(actor.parameters(), lr=lr)
 gamma = 0.99
 
 eps = 0.1
+init_logs = 0.0
+def from_log(x): 
+	# projection suggested in paper and acme impl.
+	# return F.softplus(th.clamp(x, min=-18)) + 1e-8
+	return x.exp()
 
-# check init values of lagrangian multipliers, also look into the projection etc.
-eta = nn.Parameter(th.ones(1))
-eta_optim = th.optim.Adam([eta], lr=lr)
+# check init values of lagrangian multipliers, acme impl. uses clamping to restrict the min value
+# update: apparently acme uses 1.0 as init values? these are very unstable when testing...
+log_eta = th.tensor(init_logs, requires_grad=True)
+eta_optim = th.optim.Adam([log_eta], lr=dual_lr)
+eta = from_log(log_eta).item()
 
-alpha = th.tensor(0.2, requires_grad=True)
-alpha_optim = th.optim.Adam([alpha], lr=lr)
+log_alpha = th.tensor(init_logs, requires_grad=True)
+alpha_optim = th.optim.Adam([log_alpha], lr=dual_lr)
+alpha = from_log(log_alpha).item()
 
-for t in range(10000):
+for t in range(12500):
 	batch = runner.get_batch(actor)
-	s, a, r, s_p, d = batch()
+	s, a, r, s_p, d, _ = batch()
 
 	### Policy evaluation
-	q = critic(s).gather(1, a.long())
 	with th.no_grad():
+		# ddqn
 		a_p = critic(s_p).argmax(dim=1).unsqueeze(1)		
 		q_p = targ_critic(s_p).gather(1, a_p.long())		
 		y = r + gamma * q_p * (1 - d)
+
+	q = critic(s).gather(1, a.long())
 
 	loss = F.mse_loss(q, y)
 
 	critic_optim.zero_grad()
 	loss.backward()
-	nn.utils.clip_grad_norm_(critic.parameters(), 5.0)
+	nn.utils.clip_grad_norm_(critic.parameters(), 0.5)
 	critic_optim.step()
 	
 	### E-step
 	for _ in range(5):
-		energies = th.exp(critic(s).detach() / eta).mean(-1)
+		energies = th.exp(critic(s).detach() / F.softplus(log_eta)).mean(-1)
 		logits = th.log(energies).mean()
-		eta_loss = (eta * (eps + logits))
+		eta_loss = (from_log(log_eta) * (eps + logits))
 
 		eta_optim.zero_grad()
-		eta_loss.backward()			
+		eta_loss.backward()		
+		nn.utils.clip_grad_norm_([log_eta], 40.0)	
 		eta_optim.step()
+
+	eta = from_log(log_eta).item()
 	
 	qij = F.softmax(targ_critic(s) / eta, dim=-1).detach()
 	prior_probs = targ_actor(s).detach()
@@ -119,20 +140,22 @@ for t in range(10000):
 
 		# check dimensions of kl-div!
 		kl = th.mean((probs * th.log(probs / prior_probs)).sum(dim=-1))
-		total_loss_p = -(loss_p + alpha.detach() * (eps - kl))
+		total_loss_p = -(loss_p + alpha * (eps - kl))
 
 		actor_optim.zero_grad()
 		total_loss_p.backward()
-		nn.utils.clip_grad_norm_(actor.parameters(), 5.0)
+		nn.utils.clip_grad_norm_(actor.parameters(), 0.5)
 		actor_optim.step()
 
-		# alpha currently not set to autotune
-		continue
-		alpha_loss = alpha * (eps - kl).detach()
+		# is alpha updated inside or outside of M-steps?
+		alpha_loss = from_log(log_alpha) * (eps - kl).detach()
 		alpha_optim.zero_grad()
 		alpha_loss.backward()
+		nn.utils.clip_grad_norm_([log_alpha], 40.0)
 		alpha_optim.step()
+		alpha = from_log(log_alpha).item()
 
-	# implement soft updates maybe?
-	targ_critic = copy.deepcopy(critic)
-	targ_actor = copy.deepcopy(actor)
+	# implement soft/delayed updates maybe?
+	if t % 100 == 0:	# 100 is used in acme impl.
+		targ_critic = copy.deepcopy(critic)
+		targ_actor = copy.deepcopy(actor)
