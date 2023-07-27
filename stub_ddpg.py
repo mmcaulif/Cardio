760ccc0cd29157dccd5f692405488581e1ce2f7b
mmcaulif <mmcaulif@tcd.ie>
2023-07-27 17:26:31 +0200

    syncing stubs

--- stub_ddpg.py
+++ stub_ddpg.py
@@ -7,21 +7,22 @@ from gymnasium.wrappers import RescaleAction
 
 from cardio_rl import Runner, Collector
 
-env_name = 'Pendulum-v1'
+env_name = 'MountainCarContinuous-v0'
 env = gym.make(env_name)
-env = RescaleAction(env, -1, 1)
+env = RescaleAction(env, -1.0, 1.0)
 
 runner = Runner(
 	env=env,
 	policy='whitenoise',
 	sampler=True,
-	capacity=200_000,
-	batch_size=256,
+	capacity=1_000_000,
+	batch_size=100,
 	collector=Collector(
 		env=env,
-		warmup_len=10_000,
+		warmup_len=100,
 		logger_kwargs=dict(
-	    	log_dir = env_name,
+			log_dir = env_name,
+			log_interval = 10_000
 		)
 	),
 	backend='pytorch'
@@ -32,11 +33,11 @@ class Q_critic(nn.Module):
 		super(Q_critic, self).__init__()
 
 		self.net = nn.Sequential(
-			nn.Linear(state_dim+action_dim, 400),
+			nn.Linear(state_dim+action_dim, 256),
 			nn.ReLU(),
-			nn.Linear(400, 300),
+			nn.Linear(256, 256),
 			nn.ReLU(),
-			nn.Linear(300, 1))
+			nn.Linear(256, 1))
 
 	def forward(self, state, action):
 		sa = th.concat([state, action], dim=-1)
@@ -47,32 +48,39 @@ class Policy(nn.Module):
 		super(Policy, self).__init__()
 
 		self.net = nn.Sequential(
-			nn.Linear(state_dim, 400),
+			nn.Linear(state_dim, 256),
 			nn.ReLU(),
-			nn.Linear(400, 300),
+			nn.Linear(256, 256),
 			nn.ReLU(),
-			nn.Linear(300, action_dim),
+			nn.Linear(256, action_dim),
 			nn.Tanh())
 		
 	def forward(self, state):
 		return self.net(state)
 
-critic = Q_critic(3, 1)
-actor = Policy(3, 1)
+# https://stable-baselines3.readthedocs.io/en/master/modules/ddpg.html
+critic = Q_critic(2, 1)
+actor = Policy(2, 1)
 targ_critic = copy.deepcopy(critic)
 targ_actor = copy.deepcopy(actor)
 c_optimizer = th.optim.Adam(critic.parameters(), lr=1e-3)
 a_optimizer = th.optim.Adam(actor.parameters(), lr=1e-3)
 
-for steps in range(100_000):
+for steps in range(300_000):
 	batch = runner.get_batch(actor)
 
 	s, a, r, s_p, d, _ = batch()
 
 	with th.no_grad():
 		a_p = targ_actor(s_p)
+
+		mean = th.zeros_like(a_p)
+		noise = th.normal(mean=mean, std=0.2).clamp(-0.5, 0.5)
+		
+		a_p = (a_p + noise).clamp(-1, 1)
+
 		q_p = targ_critic(s_p, a_p)
-		y = r + 0.98 * q_p * (1 - d)
+		y = r + 0.99 * q_p * (1 - d)
 
 	q = critic(s, a.squeeze(1))
 		
@@ -83,9 +91,7 @@ for steps in range(100_000):
 	th.nn.utils.clip_grad_norm_(critic.parameters(), 0.5)
 	c_optimizer.step()
 
-	a_sampled = actor(s_p)
-
-	policy_loss =  -(critic(s, a_sampled)).mean()
+	policy_loss =  -(critic(s, actor(s_p))).mean()
 
 	a_optimizer.zero_grad()
 	policy_loss.backward()
@@ -93,7 +99,7 @@ for steps in range(100_000):
 	a_optimizer.step()	
 
 	for targ_params, params in zip(targ_critic.parameters(), critic.parameters()):
-		targ_params.data.copy_(params.data * 0.01 + targ_params.data * (1.0 - 0.01))
+		targ_params.data.copy_(params.data * 0.005 + targ_params.data * (1.0 - 0.005))
 
 	for targ_params, params in zip(targ_actor.parameters(), actor.parameters()):
-		targ_params.data.copy_(params.data * 0.01 + targ_params.data * (1.0 - 0.01))
+		targ_params.data.copy_(params.data * 0.005 + targ_params.data * (1.0 - 0.005))
